1b29794855b5784c8e8c92c6d30ddc41a34c17e1
Evgenykiev <padakluev@gmx.com>
2017-12-06 20:09:06 +0200

    commit

--- js/javascript.js
+++ js/javascript.js
@@ -3,12 +3,15 @@ window.addEventListener("load", function() {
 
 
     var request = new XMLHttpRequest();
-    request.open('GET', '/my/url', true);
+    request.open('GET', 'https://wind-bow.glitch.me/twitch-api/streams/23932774784', true);
 
     request.onload = function () {
         if (this.status >= 200 && this.status < 400) {
             // Success!
             var data = JSON.parse(this.response);
+            
+            console.log(data)
+
         } else {
             // We reached our target server, but it returned an error
 
