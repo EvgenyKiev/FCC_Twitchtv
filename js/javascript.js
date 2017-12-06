e7c5a72289f047500d94b7b33edf4d403d7d8d59
Evgenykiev <padakluev@gmx.com>
2017-12-06 19:47:46 +0200

    commit

---
+++ js/javascript.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", function() {
+    console.log("All resources finished loading!");
+
+
+    var request = new XMLHttpRequest();
+    request.open('GET', '/my/url', true);
+
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            // Success!
+            var data = JSON.parse(this.response);
+        } else {
+            // We reached our target server, but it returned an error
+
+        }
+    };
+
+    request.onerror = function () {
+        // There was a connection error of some sort
+    };
+
+    request.send();
+
+
+})
