40c5a9a57f7cbc4c00c5d6a0ed1cc3790b344807
Evgenykiev <padakluev@gmx.com>
2017-12-08 20:26:30 +0200

    commit

--- js/javascript.js
+++ js/javascript.js
@@ -3,14 +3,45 @@ window.addEventListener("load", function() {
 
 
     var request = new XMLHttpRequest();
-    request.open('GET', 'https://wind-bow.glitch.me/twitch-api/streams/23932774784', true);
+    request.open('GET', 'https://wind-bow.glitch.me/twitch-api/streams/featured', true);
 
     request.onload = function () {
         if (this.status >= 200 && this.status < 400) {
             // Success!
-            var data = JSON.parse(this.response);
+            // var data = JSON.parse(this.response);
+            var data = JSON.parse(request.responseText);
             
-            console.log(data)
+            //console.log(data)
+
+            var all=data.featured[0].stream.channel
+
+
+            var channelName= data.featured[0].stream.channel.display_name
+
+            var channelLogo=data.featured[0].stream.channel.logo
+
+           var channelStatus=data.featured[0].stream.channel.status
+           var channelUrl=data.featured[0].stream.channel.url
+
+            var channelTitle=data.featured[0].title
+            var channelText=data.featured[0].text
+
+
+
+
+            //console.log(all)
+            console.log(channelName)
+            console.log(channelLogo)
+            console.log(channelStatus)
+            console.log(channelUrl)
+            console.log(channelTitle)
+            console.log(channelText)
+
+
+
+
+
+
 
         } else {
             // We reached our target server, but it returned an error
